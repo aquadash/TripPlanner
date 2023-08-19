01041c71ebc54405e1ca0a6e72c6be27847b84c3
Noah Costar <noah.costar@awaregroup.com>
2023-08-19 21:56:36 +1200

    e2e gmaps

--- AzureFunction/function_app.py
+++ AzureFunction/function_app.py
@@ -5,6 +5,7 @@ from typing import List
 import os
 import openai 
 from dotenv import load_dotenv
+from places import get_place_object, get_location_from_query, get_places_from_query, Place
 load_dotenv()
 
 openai.api_type = "azure"
@@ -26,50 +27,6 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 #     def __repr__(self):
 #         return {self.name: self.description}
 
-class Place:
-    def __init__(self, id, name, address, city, imageUrl, accessibilityFeatures, phone, description, priciness, rating, numRatings, location, openNow):
-        self.id:str = id
-        self.name:str = name
-        self.description:str = description
-        self.address:str = address
-        self.city:str = city
-        self.location:dict = location
-        self.phone:str = phone
-        self.imageUrl:str = imageUrl
-        self.accessibilityFeatures:List[str] = accessibilityFeatures
-        self.priciness:int = priciness
-        self.rating:float = rating
-        self.numRatings:int = numRatings
-        self.openNow:bool = openNow
-    
-    @classmethod
-    def from_json(cls, json_object):
-        return cls(id=json_object["place_id"], #
-                   name=json_object["name"], #
-                   address=json_object["vicinity"],
-                   city="TBC",
-                   imageUrl="https://lh3.googleusercontent.com/p/AF1QipMd50mfjQzKOgvRvx8Ll0N6RvLnisrU-C108D4F=s1360-w1360-h1020",
-                   location=json_object["geometry"]["location"], #
-                   accessibilityFeatures=[None],
-                   phone="Test",
-                   description="TBC",
-                   priciness="TEC",
-                   rating=json_object["rating"], #
-                   numRatings=json_object["user_ratings_total"], #
-                   openNow=True)
-    def to_json(self):
-        return json.dumps(self.__dict__)
-    
-    def to_dict(self):
-        return self.__dict__
-
-# example_place = Place(id= "place001", 
-#                       name = "South Australian Museum", 
-#                       address="North Terrace", city= "Adelaide",
-#                       imageUrl="https://lh3.googleusercontent.com/p/AF1QipMd50mfjQzKOgvRvx8Ll0N6RvLnisrU-C108D4F=s1360-w1360-h1020",
-#                       accessibilityFeatures=["Wheelchair-accessible car park","Wheelchair-accessible entrance","Wheelchair-accessible lift", "Wheelchair-accessible toilet"], 
-#                       phone="+61882077500", description="The South Australian Museum is a natural history museum and research institution in Adelaide, South Australia, founded in 1856 and owned by the Government of South Australia. It occupies a complex of buildings on North Terrace in the cultural precinct of the Adelaide Parklands.", priciness=0,rating=4.3,
-#                       location={"lat":-34.9205,"lng":138.6032}, numRatings= 1000, openNow=True)
 
 def get_sample_data():
     with open("sample_find_places.json", "r") as f:
@@ -82,8 +39,8 @@ def get_sample_data():
 def get_entities(query):
 
     user_input = query
-    system_msg = "A user will tell you where they want to go (geographical location), what accessibility requirements they have and what they want to do (in free text form). You need to extract place, activites and accessibility requirements (they might give multiple places, activities or accessibility requirements. Extract all of those, no hyphens, and return only one key word for each, e.g. wheelchair instead of wheelchair access. Also make all words singular form.) from user-entered text. Return those as json"
-
+    #system_msg = "A user will tell you where they want to go (geographical location), what accessibility requirements they have and what they want to do (in free text form). You need to extract the geographic location, activites and accessibility requirements (they might give multiple places, activities or accessibility requirements. Extract all of those, no hyphens, and return only one key word for each, e.g. wheelchair instead of wheelchair access. Also make all words singular form.) from user-entered text. Return those as json"
+    system_msg = "You will be given a free text query from a user, containing information about their geographic location, what they are interested in doing in this location, and their accessibility requirements. You need to extract the geographic location, activities and accessibility requirements from the user-entered text. Return those as a well formatted json object, with the following keys: location, interests, accessibility_requirements."
     messages = [
         {"role": "system", "content": system_msg},
         {"role": "user", "content": user_input}
@@ -111,7 +68,7 @@ def get_entities(query):
     )
     extracted_activities = response.choices[0].message['content']
 
-    return "User says: "+"\n"+"Output: "+extracted_activities+"\n"
+    return json.loads(extracted_activities)
         
 
 @app.route(route="places", methods=["GET"])
@@ -128,6 +85,10 @@ def Places(req: func.HttpRequest) -> func.HttpResponse:
             query = req_body.get('query')
     
     if query:
+        entities = get_entities(query)
+        location: dict = get_location_from_query(entities["location"])
+        places: List[str] = get_places_from_query(entities["interests"], location)
+        place_objects: List[Place] = [get_place_object(place).to_dict() for place in places]
         # 1. process query
         # 1.1 Open AI query - get location, interests, etc.
 
@@ -145,8 +106,7 @@ def Places(req: func.HttpRequest) -> func.HttpResponse:
         # 4.3 Return places as JSON
         # print(get_entities(query))
         return_object = {
-        "extracted_oai": get_entities(query),
-        "places": get_sample_data()
+        "places": place_objects,
         }
         return func.HttpResponse(json.dumps(return_object), mimetype="application/json",status_code=200)
     else:
--- AzureFunction/places.py
+++ AzureFunction/places.py
@@ -1,21 +1,65 @@
 import os
 import requests
+import json
 from dotenv import load_dotenv
-from typing import Optional
+from typing import Optional, List, Union
 
 load_dotenv()
 
 maps_key = os.getenv("GOOGLE_MAPS_API_KEY")
 
+fields_places = "business_status,place_id,name,formatted_address,editorial_summary,formatted_phone_number,geometry,photos,price_level,rating,types,website,wheelchair_accessible_entrance,user_ratings_total,opening_hours"
 
 find_location = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={input}&inputtype=textquery&fields=place_id,name,geometry&key={maps_key}"
-search_place_text = "https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&fields=place_id,name&key={maps_key}"
-get_place_details_url = "https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&key={maps_key}"
+search_place_text = "https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&key={maps_key}"
+get_place_details_url = "https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields={fields}&key={maps_key}"
 
 
 find_place_nearby = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=restaurant&name=harbour&key=AIzaSyBR85dl_oSCesioQp29jOEWJYgDsgNcSOM"
 
-#get_place_photo_url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo_reference}&key={maps_key}"
+
+class Place:
+    def __init__(self, id, name, description, address, location, imageReference, phone, wheelchair_accessible_entrance, priciness, rating, numRatings, openNow,status,website,types):
+        self.id:str = id
+        self.name:str = name
+        self.description:str = description
+        self.address:str = address
+        self.location:dict = location
+        self.phone:str = phone
+        self.imageReference:str = imageReference
+        self.wheelchair_accessible_entrance:List[str] = wheelchair_accessible_entrance
+        self.priciness:int = priciness
+        self.rating:float = rating
+        self.numRatings:int = numRatings
+        self.openNow:bool = openNow
+        self.status:str = status
+        self.website:str = website
+        self.types:List[str] = types
+    
+    @classmethod
+    def from_json(cls, place_result: dict):
+        return cls(id=place_result.get("place_id", None),
+                 name = place_result.get("name", None),
+                 description=place_result.get("editorial_summary",{}).get('overview',{}),
+                 address=place_result.get("formatted_address"),
+                 location=place_result.get("geometry",{}).get("location",{}),
+                 imageReference=place_result.get("photos",[{}])[0].get("photo_reference",None),
+                 phone=place_result.get("formatted_phone_number",None),
+                 wheelchair_accessible_entrance=place_result.get("wheelchair_accessible_entrance",None),
+                 priciness=place_result.get("price_level",None),
+                 rating=place_result.get("rating",None),
+                 numRatings=place_result.get("user_ratings_total",None),
+                 openNow=place_result.get("opening_hours",{}).get("open_now",None),
+                 status=place_result.get("business_status",None),
+                 website=place_result.get("website",None),
+                 types=place_result.get("types",None))
+    
+    def to_json(self):
+        return json.dumps(self.__dict__)
+    
+    def to_dict(self):
+        return self.__dict__
+    
 
 def get_place_details(place_id: str):
     response = requests.get(get_place_details_url.format(place_id=place_id, maps_key=maps_key)).json()
@@ -31,10 +75,25 @@ def get_location_from_query(location: str):
     else:
         return None
 
-def get_places_from_query(query: str, location: Optional[dict]):
+def get_place_object(place_result: tuple):
+    place_id = place_result[0]
+    place_name = place_result[1]
+    result = requests.get(get_place_details_url.format(place_id=place_id, fields=fields_places, maps_key=maps_key)).json()
+    if result.get("status") == "OK":
+        place_result = result["result"]
+        assert place_result["place_id"] == place_id and place_result["name"] == place_name
+    return Place.from_json(place_result)
+
+
+def get_places_from_query(query: Union[str, List[str]], location: Optional[dict]):
+    if isinstance(query, list):
+        query = "+".join(query)
     if location:
-        query += f"&location={location['lat']}%{location['lng']}"
-    response = requests.get(search_place_text.format(input=query, maps_key=maps_key)).json()
+        query += f"&location={location['lat']}%2C{location['lng']}"
+    #return query
+    response = requests.get(search_place_text.format(query=query, 
+                                                     maps_key=maps_key,)
+                                                     ).json()
     if response.get("status") == "OK":
         places = response["results"]
-    return [place["place_id"] for place in places if place['rating'] > 3.0] # hardcoded rating limit lol
+    return [(place['place_id'],place['name']) for place in places if place['rating'] > 3.0] # hardcoded rating limit lol (place["place_id"], place['name'])
