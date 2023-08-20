9a31b772375d69255cd68e16edd50f64d8cc9652
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2023-08-20 14:41:15 +1000

    updated navbar links

--- app/src/navbar/Navbar.tsx
+++ app/src/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 font-mono">
       <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center">
           <img src="TripPlanner.png" className="h-8 mr-3" alt="Flowbite Logo" />
@@ -36,7 +36,9 @@ const Navbar = () => {
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a
-                href="https://hackerspace.govhack.org"
+                href="https://hackerspace.govhack.org/projects/tourmate"
+                target="_blank"
+                rel="noreferrer"
                 className="block py-2 pl-3 pr-4 text-white bg-[#0393A1] rounded md:bg-transparent md:text-[#0393A1] md:p-0 dark:text-white md:dark:text-[#0393A1]"
                 aria-current="page"
               >
@@ -46,6 +48,8 @@ const Navbar = () => {
             <li>
               <a
                 href="https://github.com/aquadash/TripPlanner"
+                target="_blank"
+                rel="noreferrer"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#0393A1] md:p-0 dark:text-white md:dark:hover:text-[#0393A1] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 GitHub
